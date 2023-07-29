30715023e7e217fbf1874cf754c0d1d0f0fd197a
Priyanka03012005 <127594944+Priyanka03012005@users.noreply.github.com>
2023-07-29 22:22:35 +0530

    Update ValoAdapter.kt

--- app/src/main/java/com/example/valoapi/ValoAdapter.kt
+++ app/src/main/java/com/example/valoapi/ValoAdapter.kt
@@ -8,28 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class ValoAdapter(val agents:ValoAgents) :
-    RecyclerView.Adapter<ValoAdapter.ValoViewHolder>() {
-
-    lateinit var context: Context
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ValoViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
-        return ValoViewHolder(view)
+class ValoAdapter(val agents: ValoAgents) :
+    RecyclerView.Adapter<ValoAdapter.ViewHolder>() {
+    lateinit var context:Context
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        var agents: TextView = view.findViewById(R.id.agentName)
+        var img: ImageView = view.findViewById(R.id.agentImage)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        context = parent.context
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.agents.text = agents.data[position].displayName
+        Glide.with(context).load(agents.data[position].displayIconSmall).into(holder.img)
+        holder.img.setOnClickListener{
+            Toast.makeText(context,agents.data[position].displayName,Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
         return agents.data.size
-    }
-
-    override fun onBindViewHolder(holder: ValoViewHolder, position: Int) {
-        holder.agents.text = agents.data[position].displayName
-        Glide.with(context).load(agents.data[position].displayIcon).into(holder.img)
-    }
-    class ValoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        var img = view.findViewById<ImageView>(R.id.agentImage)
-        var agents = view.findViewById<TextView>(R.id.agentName)
     }
 }
