e4181e0fcfb56c63ee17d7c142d429020915772e
Priyanka03012005 <127594944+Priyanka03012005@users.noreply.github.com>
2023-07-29 18:35:18 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/valoapi/MainActivity.kt
+++ app/src/main/java/com/example/valoapi/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.valoapi
 
+import ValoAgents.ValoAgents
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import com.example.valoapi.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getValoAgents()
+    }
+
+    private fun getValoAgents() {
+        val valoAgents = APIService.api.AgentsInterface()
+        valoAgents.enqueue(object : Callback<ValoAgents>{
+            override fun onFailure(call: Call<ValoAgents>, t: Throwable) {
+                Log.d("VALORANT","Error in Fetching List",t)
+            }
+
+            override fun onResponse(
+                call: Call<ValoAgents>,
+                response: Response<ValoAgents>
+            ) {
+                val valoAgents = response.body()
+                if(valoAgents!=null){
+                  Log.d("VALORANT",valoAgents.toString())
+//                    if(response.isSuccessful) {
+//                        val res = response.body()
+//                        binding.recyclerview.adapter = res?.let { ValoAdapter(it) }
+//                    }
+
+                }
+            }
+        })
     }
 }
